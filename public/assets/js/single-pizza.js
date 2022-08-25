dac6d61ee02d3f15f27073da096882796b3c5738
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-08-25 18:31:24 -0500

    add replies complete

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -8,26 +8,26 @@ const $commentSection = document.querySelector('#comment-section');
 const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
-// function getPizza() {
-//   // get id of Pizza
-//   const searchParams = new URLSearchParams(document.location.search.substring(1));
-//   const pizzaId = searchParams.get('id');
+function getPizza() {
+  // get id of Pizza
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id');
 
-//   // get pizzaInfo
-//   fetch(`/api/pizzas/${pizzaId}`)
-//   .then(response => {
-//     // check for a 4xx or 5xx error from server
-//     if(!response.ok){
-//       throw new Error({message: 'something went wrong!'})
-//     }
-//     return response.json();
-//   }).then(printPizza)
-//   .catch(err => {
-//     console.log(err);
-//     alert('Cannot find a pizza with this id! Taking you back!');
-//     window.history.back();
-//   });
-// }
+  // get pizzaInfo
+  fetch(`/api/pizzas/${pizzaId}`)
+  .then(response => {
+    // check for a 4xx or 5xx error from server
+    if(!response.ok){
+      throw new Error({message: 'something went wrong!'})
+    }
+    return response.json();
+  }).then(printPizza)
+  .catch(err => {
+    console.log(err);
+    alert('Cannot find a pizza with this id! Taking you back!');
+    window.history.back();
+  });
+}
 function printPizza(pizzaData) {
   console.log(pizzaData);
 
@@ -106,6 +106,26 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    response.json();
+  }).then(commentResponse => {
+    console.log(commentResponse);
+    location.reload();
+  }).catch(err => {
+    console.log(err);
+  });
 }
 
 function handleNewReplySubmit(event) {
@@ -125,6 +145,27 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 $backBtn.addEventListener('click', function() {
@@ -133,4 +174,4 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
-// getPizza(); 
+getPizza(); 
